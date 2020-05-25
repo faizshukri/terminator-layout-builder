afca61608b25f14257245e81084eee0c8be1b541
Faiz Shukri <faizshukri90@gmail.com>
2020-05-26 07:46:36 +0800

    Add more properties and error handler
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from configobj import ConfigObj
 import yaml
 import pydash
 import json
+import sys
+import numbers
 
 
 @dataclass
@@ -44,18 +46,17 @@ class Window(Split, Terminal):
 
 Layout = Dict[str, List[Window]]
 
-_last_id = 0
-
 config = ConfigObj(indent_type="  ")
 
 config.filename = "./hey.ini"
 
+_ID = 0
+
 
 def next_id(name):
-    global _last_id
-    id = _last_id
-    _last_id += 1
-    return '%s%s' % (name, id)
+    global _ID
+    _ID += 1
+    return '%s%s' % (name, _ID)
 
 
 def readConfig(file: str) -> Layout:
@@ -86,6 +87,10 @@ def resolveElem(elem, parentElem):
                    'command': 'bash -c "trap $SHELL EXIT; %s"' % cmd, 'profile': 'default'}
         if elem.get('order') is not None:
             cmdElem['order'] = elem.get('order')
+
+        if elem.get('title'):
+            cmdElem['title'] = elem.get('title')
+
         return {next_id('terminal'): cmdElem}
 
     if any(k in elem for k in ("vertical", "horizontal")):
@@ -93,12 +98,29 @@ def resolveElem(elem, parentElem):
         panedType = 'HPaned' if "horizontal" in elem else 'VPaned'
         panedElem = {"parent": parentElem['id'],
                      "type": panedType, "id": next_id(panedType.lower())}
-        if elem.get('order'):
+
+        verticalOrHorinzontalElem = (
+            elem.get('vertical') or elem.get('horizontal'))
+
+        if elem.get('order') is not None:
             panedElem['order'] = elem.get('order')
+
+        ratio = verticalOrHorinzontalElem.get('ratio')
+        if ratio:
+            if(not isinstance(ratio, numbers.Number) or ratio < 0 or ratio > 1):
+                sys.exit(
+                    "[ERROR] Ratio must be a Number, between 0 and 1.")
+
+            panedElem['ratio'] = ratio
+
         pydash.assign(template, {panedElem['id']: panedElem})
-        panes = (elem.get('vertical')
-                 or elem.get('horizontal')).get('panes')
-        lala = list(map(lambda x: resolveElem(x, panedElem), panes))
+
+        panes = verticalOrHorinzontalElem.get('panes')
+        if(len(panes) != 2):
+            sys.exit(
+                "[ERROR] Panes must have exactly 2 items.")
+        lala = list(map(lambda i_el: resolveElem(pydash.assign(
+            i_el[1], {"order": i_el[0]}), panedElem), enumerate(panes)))
         for key, value in [(k, v) for x in lala for (k, v) in x.items()]:
             pydash.assign(template, {key: value})
         return template
@@ -107,7 +129,14 @@ def resolveElem(elem, parentElem):
     if notebook:
         template = {}
         notebookEl = {"parent": parentElem.get('id'), "type": "Notebook",
-                      "id": next_id('notebook')}
+                      "id": next_id('notebook'), "active_page": 0}
+
+        if notebook.get('labels'):
+            if(len(notebook.get('labels')) != len(notebook.get('tabs'))):
+                sys.exit(
+                    "[ERROR] The labels must be the same size with the tabs.")
+            notebookEl['labels'] = notebook.get('labels')
+
         pydash.assign(template, {notebookEl['id']: notebookEl})
         lala = list(map(lambda i_el: resolveElem(pydash.assign(
             i_el[1], {"order": i_el[0]}), notebookEl), enumerate(notebook['tabs'])))
@@ -115,9 +144,11 @@ def resolveElem(elem, parentElem):
             pydash.assign(template, {key: value})
         return template
 
+    sys.exit("[ERROR] Unknown element: %s" % elem)
+
 
 def main():
-    for layout, windows in readConfigTest('data')[3].items():
+    for layout, windows in readConfigTest('data')[4].items():
         for window in windows:
             print(window)
 
